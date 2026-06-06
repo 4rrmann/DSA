95634df3c8d57966c353e20e3e930a5999e88f5b
Arman Ahmad <aru44362@gmail.com>
2026-06-06 22:42:22 +0530

    IImplementing Linear Search

---
+++ dsa.py/List-ArrayQuestions/08linearSearch.py
@@ -0,0 +1,12 @@
+nums = [1, 3, 4, 5, 2, 6, 9]
+
+def ls(target): 
+    n = len(nums)
+
+    for i in range(0, n):
+        if nums[i] == target:
+            return i
+        
+    return -1
+
+print(ls(10))
